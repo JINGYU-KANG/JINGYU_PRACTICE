16b275c60014b0c1d31545672d5c088331f8bb09
jin <jingyukang91@gmail.com>
2021-09-08 21:52:25 +1200

    HooksTictactoe last commit

--- components/HooksTictactoe/Table.jsx
+++ components/HooksTictactoe/Table.jsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import Tr from "./Tr";
 
-const Table = ({ onClick, tableData, dispatch }) => {
+const Table = React.memo(({ tableData, dispatch }) => {
   console.log("table rendered");
 
+  // const ref = useRef([]);
+  // useEffect(() => {
+  //   console.log(tableData === ref.current[0], dispatch === ref.current[1]);
+  //   ref.current = [tableData, dispatch];
+  // }, [tableData, dispatch]);
+
+  console.log("tfcome");
   return (
     <table>
       {Array(tableData.length)
         .fill()
         .map((tr, i) => (
-          <Tr rowIndex={i} rowData={tableData[i]} dispatch={dispatch} />
+          <Tr key={i} rowIndex={i} rowData={tableData[i]} dispatch={dispatch} />
         ))}
     </table>
   );
-};
+});
 
 export default Table;
